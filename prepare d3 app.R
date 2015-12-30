40f18530ca5333b724a0efd6e32ef40eea3b1c0c
wuergh <erle.holgersen@gmail.com>
2015-12-30 09:33:35 +0100

    Added 2005 and 2009

--- prepare d3 app.R
+++ prepare d3 app.R
@@ -1,3 +1,5 @@
+# 2013
+
 data <- read.csv2("valgdata2013.csv", encoding = "UTF-8")
 data <- data[data$Parti != "Andre2",]
 
@@ -6,3 +8,27 @@ stemmedata <- aggregate(Stemmer.totalt ~ Parti + Fylkenavn, data, FUN = sum)
 names(stemmedata) <- c("Parti", "Fylke", "Stemmer")
 
 write.csv(stemmedata, "d3/2013.csv", row.names = FALSE)
+
+
+# 2009
+
+data <- read.csv2("valgdata2009.csv", encoding = "UTF-8")
+data <- data[data$Parti != "Andre2",]
+
+stemmedata <- aggregate(StemmerTotalt ~ Parti + Fylkenavn, data, FUN = sum)
+
+names(stemmedata) <- c("Parti", "Fylke", "Stemmer")
+
+write.csv(stemmedata[-1,], "d3/2009.csv", row.names = FALSE)
+
+
+
+# 2005
+data <- read.csv("valgdata2005.csv", encoding = "UTF-8")
+data <- data[data$Parti != "Andre2",]
+
+stemmedata <- aggregate(StemmerTotalt ~ Parti + Fylkenavn, data, FUN = sum)
+
+names(stemmedata) <- c("Parti", "Fylke", "Stemmer")
+
+write.csv(stemmedata[-1,], "d3/2005.csv", row.names = FALSE)
